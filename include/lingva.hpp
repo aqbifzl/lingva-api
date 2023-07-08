1d04fdbe92e19b9ba295a5546093ef17e37e3678
aqbifzl <aqbifzl@icloud.com>
2023-07-08 03:23:10 +0200

    ✨ Create header-only library

---
+++ include/lingva.hpp
@@ -0,0 +1,86 @@
+#ifndef __LINGVA_API__
+#define __LINGVA_API__
+#include <curl/curl.h>
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include <string>
+#include <vector>
+
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb,
+                            void *userp) {
+  ((std::string *)userp)->append((char *)contents, size * nmemb);
+  return size * nmemb;
+}
+
+class lingvaClient {
+public:
+  lingvaClient(std::string source = "auto", std::string target = "en",
+               std::string lingvaInstance = "lingva.ml")
+      : source(source), target(target), lingvaInstance(lingvaInstance) {
+    curl = curl_easy_init();
+  }
+
+  std::vector<std::pair<std::string, std::string>> languages;
+
+  void listLanguages() {
+    if (languages.empty())
+      getLanguages();
+    for (const auto &lang : languages) {
+      std::cout << lang.first << " - " << lang.second << '\n';
+    }
+  }
+
+  void getLanguages() {
+    std::string url = "https://" + lingvaInstance + "/api/v1/languages";
+
+    nlohmann::json result = makeRequest(url);
+    try {
+      for (const auto &lang : result["languages"]) {
+        languages.push_back(
+            {lang["name"].get<std::string>(), lang["code"].get<std::string>()});
+      }
+    } catch (const nlohmann::json::exception &e) {
+      std::cerr << "JSON parsing error: " << e.what() << std::endl;
+    }
+  }
+
+  std::string translate(std::string text, std::string cSource = "auto",
+                        std::string cTarget = "en") {
+    source = cSource;
+    target = cTarget;
+
+    std::string url =
+        "https://" + lingvaInstance + "/api/v1/" + source + '/' + target + "/";
+    url.append(curl_easy_escape(curl, text.c_str(), text.length()));
+
+    try {
+      nlohmann::json result = makeRequest(url);
+      return result["translation"];
+    } catch (const nlohmann::json::exception &e) {
+      std::cerr << "JSON parsing error: " << e.what() << std::endl;
+    }
+    return "";
+  }
+
+private:
+  nlohmann::json makeRequest(std::string url) {
+    if (curl) {
+      std::string result = "";
+      CURLcode res;
+      curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+      curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+      curl_easy_setopt(curl, CURLOPT_WRITEDATA, &result);
+
+      res = curl_easy_perform(curl);
+      return nlohmann::json::parse(result);
+      curl_easy_cleanup(curl);
+    }
+    return "";
+  }
+  std::string source;
+  std::string lingvaInstance;
+  std::string target;
+  CURL *curl;
+};
+
+#endif
